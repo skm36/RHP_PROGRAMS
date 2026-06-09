d73894c86bc895f0adff251441ee0d8e5864e134
skm36 <240302.EC@rmkec.ac.in>
2026-06-09 15:51:21 +0530

    Update Dynamic_Prog1.cpp

--- Dynamic_Prog1.cpp
+++ Dynamic_Prog1.cpp
@@ -1,51 +1,38 @@
 #include <iostream>
-#include <climits>
 using namespace std;
 int r,c;
-int* fsmax(int a[][1000],int row,int c){
-    int fm = INT_MIN, sm = INT_MIN;
-    int idx = -1;
-    int *b = new int[3];
-    for(int i=0;i<c;i++){
-        if(a[row][i] > fm){
-            sm = fm;
-            fm = a[row][i];
-            idx = i;
-        }
-        else if(a[row][i] > sm){
-            sm = a[row][i];
-        }
+int* fsmax(int a[][1000],int c,int r){
+    int fm=0,sm=0;
+    int *b = new int[2];
+    for(int i=0;i<r;i++){
+        if(a[i][c]>fm){
+                sm = fm;
+                fm  = a[i][c];
+            }else if(a[i][c]>sm)
+            sm=a[i][c];
     }
-    b[0] = fm;
-    b[1] = sm;
-    b[2] = idx;
+    b[0]=fm;
+    b[1]=sm;
     return b;
 }
 int main() {
-    cin >> r >> c;
-    int a[1000][1000], dp[1000][1000];
+    cin>>r>>c;
+    int a[r][c],dp[1000][1000];
+    int fm=0,sm=0;
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
-            cin >> a[i][j];
-            if(i==0)
-               dp[i][j] = a[i][j];
+            cin>>a[i][j];
+            if(j==0)
+            dp[i][j]=a[i][j];
         }
     }
-    for(int i=1;i<r;i++){
-        int* y = fsmax(dp,i-1,c);
-        for(int j=0;j<c;j++){
-            dp[i][j] = a[i][j] + ((j == y[2]) ? y[1] : y[0]);
+    for(int i=1;i<c;i++){
+        int* y = fsmax(dp,i-1,r);
+        for(int j=0;j<r;j++){
+           dp[j][i] = a[j][i] +((dp[j][i-1] ==y[0])?y[1]:y[0]);
         }
     }
-    cout << fsmax(dp,r-1,c)[0];
+    cout<<fsmax(dp,c-1,r)[0];
 
     return 0;
 }
-
-
-
-
-
-
-
-
